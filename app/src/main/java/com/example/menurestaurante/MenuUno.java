0db228ff47cfe069442c892e65ddc596c04e7224
Itza Kevin <52048588+Itza-Kevin@users.noreply.github.com>
2023-06-22 22:49:27 -0600

    cambio de pantalla

--- app/src/main/java/com/example/menurestaurante/MenuUno.java
+++ app/src/main/java/com/example/menurestaurante/MenuUno.java
@@ -3,6 +3,7 @@ package com.example.menurestaurante;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuUno extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MenuUno extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_uno);
+    }
+    public void retorna(View vista){
+        finish();
     }
 }
