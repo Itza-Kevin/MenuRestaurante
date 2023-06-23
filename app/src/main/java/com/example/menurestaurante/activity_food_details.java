b11960b8d840c6bebe87c75b36f445c57e5caf09
Itza Kevin <52048588+Itza-Kevin@users.noreply.github.com>
2023-06-23 00:02:41 -0600

    accion cambio de platillo

--- app/src/main/java/com/example/menurestaurante/activity_food_details.java
+++ app/src/main/java/com/example/menurestaurante/activity_food_details.java
@@ -2,7 +2,9 @@ package com.example.menurestaurante;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_food_details extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class activity_food_details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_details);
+    }
+    public void platillouno(View V){
+        Intent intento=new Intent(this, activity_food_details.class);
+        startActivity(intento);
+
     }
 }
